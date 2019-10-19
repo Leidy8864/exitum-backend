8541a1aa1c67cf487d09eb5c6aeb9da1f59cfef6
RonaldMiya <ronald.miya@tecsup.edu.pe>
2019-10-18 20:03:57 -0500

    Database [dev]

--- ScriptExitum.Vx.sql
+++ ScriptExitum.Vx.sql
@@ -5,7 +5,7 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema exitum
 -- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Schema exitum
@@ -1179,9 +1179,9 @@ CREATE TABLE IF NOT EXISTS `exitum`.`schedule` (
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `mydb`.`workshop`
+-- Table `exitum`.`workshop`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`workshop` (
+CREATE TABLE IF NOT EXISTS `exitum`.`workshop` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(145) NULL,
   `description` TEXT NULL,
@@ -1196,7 +1196,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`workshop` (
   INDEX `fk_workshop_user1_idx` (`user_id` ASC),
   CONSTRAINT `fk_workshop_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `mydb`.`user` (`id`)
+    REFERENCES `exitum`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
