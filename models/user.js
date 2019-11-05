cc1e0a7f2ba161fc35d10b6e3673eb41e77b6512
leidy8864 <leidy.callupe@tecsup.edu.pe>
2019-11-05 11:09:21 -0500

    use model fix

--- models/user.js
+++ models/user.js
@@ -1,59 +1,57 @@
 'use strict';
 
-const Sequelize = require('sequelize')
-
-
 module.exports = (sequelize, DataType) => {
-  const user = sequelize.define('user', {
-    id: { type: DataType.INTEGER, primaryKey: true, autoIncrement: true },
-    name: DataType.STRING,
-    lastname: DataType.STRING,
-    email: {
-      type: DataType.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    provider_id: DataType.STRING,
-    confirmed: DataType.BOOLEAN,
-    phone: DataType.STRING,
-    role: {
-      type: DataType.ENUM,
-      values: ['entrepreneur', 'employee', 'admin','undefined'],
-      allowNull: false
-    },
-    method: {
-      type: DataType.ENUM,
-      values: ['local', 'google', 'facebook'],
-      allowNull: false
-    },
-    password : DataType.STRING,
-    active: DataType.BOOLEAN,
-    last_login: DataType.DATE,
-    photo: DataType.STRING,
-    photo_dni: DataType.STRING,
-    avg_rating: DataType.FLOAT(3, 2),
-    country_id: {
-      type: DataType.INTEGER,
-      references: {
-        model: 'country',
-        key: 'id'
+  const user = sequelize.define(
+    'user',
+    {
+      id: { type: DataType.INTEGER, primaryKey: true, autoIncrement: true },
+      name: DataType.STRING,
+      lastname: DataType.STRING,
+      email: {
+        type: DataType.STRING,
+        allowNull: false,
+        unique: true
+      },
+      provider_id: DataType.STRING,
+      confirmed: DataType.BOOLEAN,
+      phone: DataType.STRING,
+      role: {
+        type: DataType.ENUM,
+        values: ['entrepreneur', 'employee', 'admin', 'undefined'],
+        allowNull: false
+      },
+      method: {
+        type: DataType.ENUM,
+        values: ['local', 'google', 'facebook'],
+        allowNull: false
+      },
+      password: DataType.STRING,
+      active: DataType.BOOLEAN,
+      last_login: DataType.DATE,
+      photo: DataType.STRING,
+      photo_dni: DataType.STRING,
+      avg_rating: DataType.FLOAT(3, 2),
+      country_id: {
+        type: DataType.INTEGER,
+        references: {
+          model: 'country',
+          key: 'id'
+        }
+      },
+      currency_id: {
+        type: DataType.INTEGER,
+        references: {
+          model: 'currency',
+          key: 'id'
+        }
       }
     },
-    currency_id: {
-      type: DataType.INTEGER,
-      references: {
-        model: 'currency',
-        key: 'id'
-      }
-    },
-    from_hour: Sequelize.TIME,
-    to_hour: Sequelize.TIME,
-  },
     {
       freezeTableName: true,
       timestamps: false,
       omitNull: true
-    })
+    }
+  );
   user.associate = (models) => {
     user.hasOne(models.entrepreneur, {
       foreignKey: 'user_id'
@@ -66,7 +64,7 @@ module.exports = (sequelize, DataType) => {
     });
     user.belongsTo(models.currency, {
       foreignKey: 'currency_id'
-    })
+    });
     user.hasOne(models.token, {
       foreignKey: 'user_id'
     });
@@ -112,38 +110,32 @@ module.exports = (sequelize, DataType) => {
       foreignKey: 'user_id',
       otherKey: 'startup_id'
     });
-    user.hasMany(models.unavailable, { as: 'unavailables' });
+    user.hasMany(models.available, {
+      as: 'toAvailable'
+    });
     user.hasMany(models.workshop, {
       foreignKey: 'user_id'
     });
-    user.belongsToMany(models.user, {
-      as: { singular: 'fromUserAppointment', plural: 'fromUserAppointments' },
-      through: models.appointment,
-      foreignKey: 'from_user_id',
-      otherKey: 'to_user_id'
-    })
-    user.belongsToMany(models.user, {
-      as: { singular: 'toUserAppointment', plural: 'toUserAppointments' },
-      through: models.appointment,
-      foreignKey: 'to_user_id',
-      otherKey: 'from_user_id'
-    })
+    user.hasMany(models.challenge, {
+      foreignKey: 'user_id'
+    });
+    user.hasMany(models.certification, {
+      foreignKey: 'user_id'
+    });
+    user.hasMany(models.experience, {
+      as: 'experience',
+      foreignKey: 'user_id'
+    });
+    user.hasMany(models.education, {
+      as: 'education',
+      foreignKey: 'user_id'
+    });
     user.belongsToMany(models.skill, {
       as: { singular: 'toUserSkill', plural: 'toUserSkills' },
       through: models.skill_user,
       foreignKey: 'user_id',
       otherKey: 'skill_id'
     });
-  }
-  // Object.defineProperty(user.prototype, 'fullName', {
-  //   get() {
-  //     return this.name + ' ' + this.name
-  //   },
-  //   set(name) {
-  //     const names = name.split(' ')
-  //     this.lastname = names.pop()
-  //     this.name = names.join(' ')
-  //   }
-  // })
-  return user
-};
+  };
+  return user;
+}
