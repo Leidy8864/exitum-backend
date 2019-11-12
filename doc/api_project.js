c7aa415b76a3e706250ed8692d4e4bdfa5abcfd1
leidy8864 <leidy.callupe@tecsup.edu.pe>
2019-11-12 11:19:47 -0500

    fix documentation

--- doc/api_project.js
+++ doc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-11-12T16:16:52.048Z",
+    "time": "2019-11-12T16:19:30.761Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
