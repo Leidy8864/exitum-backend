bc38f648f9d5d306f1949956bb49b7ad1db62f2b
leidy8864 <leidy.callupe@tecsup.edu.pe>
2019-11-25 10:11:37 -0500

    advice fix

--- models/advice.js
+++ models/advice.js
@@ -1,11 +1,13 @@
 'use strict'
+const Sequelize = require('sequelize');
 
 module.exports = (sequelize, DataType) => {
-    const advice = sequelize.define('advice', {
-        id: { type: DataType.INTEGER, primaryKey: true, autoIncrement: true },
-        title: DataType.STRING,
-        description: DataType.STRING
-    },
+    const advice = sequelize.define('advice',
+        {
+            id: { type: DataType.INTEGER, primaryKey: true, autoIncrement: true },
+            title: DataType.STRING,
+            description: DataType.STRING
+        },
         {
             freezeTableName: true,
             timestamps: false,
