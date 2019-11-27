7314cd196b78d09321ee8e631b57447e639d544c
RonaldMiya <ronald.miya@tecsup.edu.pe>
2019-11-27 14:30:06 -0500

    Delete event [dev]

--- doc/api_project.js
+++ doc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-11-26T17:26:18.943Z",
+    "time": "2019-11-27T19:29:25.212Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
