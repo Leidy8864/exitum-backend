20e759a1331d8a8862e63fd5d9d6850e954f370e
RonaldMiya <ronald.miya@tecsup.edu.pe>
2019-11-27 17:59:53 -0500

    List all events [dev]

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -40,15 +40,33 @@ module.exports = {
 
         let perPage = 20;
         let page = req.query.page || 1;
+        // let user = req.query.user
 
         try {
+
+            var events_number = await models.workshop.count()
             
             var events = await models.workshop.findAll({
                 offset: (perPage * (page - 1)),
-                limit: perPage
+                limit: perPage,
+                attributes: [
+                    'id', 'title', 'day',  [ Sequelize.fn( 'TIME_FORMAT', Sequelize.col('hour_start'),  '%h:%i %p'), 'hour_start' ],
+                    [ Sequelize.fn( 'TIME_FORMAT', Sequelize.col('hour_end'),  '%h:%i %p'), 'hour_end' ], 'place'
+                ],
+                include: [
+                    {
+                        model: models.user,
+                        as: 'toWorkshopUsers',
+                        attributes:[ 'id', [ Sequelize.fn('CONCAT', Sequelize.col('toWorkshopUsers.name'), ' ', Sequelize.col('lastname')), 'fullname' ], 'photo' ]
+                    },
+                    {
+                        model: models.category,
+                        as: 'toWorkshopCategories'
+                    }
+                ]
             })
 
-            res.status(200).json({ status: true, message: 'OK', data: events, current: page, pages: Math.ceil(events.length / perPage) })
+            return res.status(200).json({ status: true, message: 'OK', data: events, current: page, pages: Math.ceil(events_number / perPage) })
 
         } catch (error) { returnError(res, error) }
 
@@ -70,7 +88,7 @@ module.exports = {
                 where: { user_id: user.id }
             })
 
-            res.status(200).json({ status: true, message: 'OK', data: events, current: page, pages: Math.ceil(events.length / perPage) })
+            return res.status(200).json({ status: true, message: 'OK', data: events, current: page, pages: Math.ceil(events.length / perPage) })
 
         } catch (error) { returnError(res, error) }
 
