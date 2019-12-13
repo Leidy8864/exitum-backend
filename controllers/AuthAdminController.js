7666914965fd82b7acfc49a10f0f6d428bf97372
RonaldMiya <ronald.miya@tecsup.edu.pe>
2019-12-13 16:00:00 -0500

    Login token [dev]

--- controllers/AuthAdminController.js
+++ controllers/AuthAdminController.js
@@ -88,7 +88,7 @@ module.exports = {
     me: (req, res) => {
         try 
         {
-            return res.status(200).json({ status: true, data: req.admin })
+            return res.status(200).json({ status: true, data: req.user })
 
         } catch (error) {
             return res.status(500).json({error: error})
