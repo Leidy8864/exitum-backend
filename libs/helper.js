663dde51870320626db688d08ca18ce9ba806758
Jose Cristobal <jose.cristobal@tecsup.edu.pe>
2019-12-28 03:19:14 -0500

    alerta de challenges y tiempo expiración token

--- libs/helper.js
+++ libs/helper.js
@@ -21,11 +21,11 @@ module.exports = {
     },
 
     generateAccessData: function (user, res) {
-        // const expiresIn = 24*60*60;
+        const expiresIn = 12*60*60;
 
         const accessToken = jwt.sign({ id: user.id },
             SECRET_KEY, {
-            // expiresIn : expiresIn
+            expiresIn : expiresIn
         });
 
         const accessData = {
@@ -42,7 +42,7 @@ module.exports = {
             // expiresIn : expiresIn
         }
 
-        return res.status(200).json({ status: true, message: "Ok", data: accessData });
+        return res.status(200).json({ status: true, message: "OK", data: accessData });
     },
 
     accessData: function (user, res) {
